512c194d826112895f0ba9b724a8800b4594dd56
Adrien BLANCHET <adrien.blanchet@live.fr>
2021-11-25 20:48:30 +0100

    Attempt to compile with Borealis

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-switch-browser
 Simple file browser for the Nintendo Switch
+
+```shell
+sudo dkp-pacman \
+  -r /System/Volumes/Data \
+  -S deko3d switch-glfw switch-mesa switch-glm
+```
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,35 @@
 // Created by Adrien BLANCHET on 25/11/2021.
 //
 
-#include "stdlib.h"
+#include <cstdlib>
+
+#include "switch.h"
+
+#include "borealis.hpp"
+#include "Logger.h"
+#include "VersionConfig.h"
+
+LoggerInit([]{
+  Logger::setUserHeaderStr("[SimpleSwitchBrowser]");
+})
 
 int main(int argc_, char** argv_){
+  LogInfo << "Software version: " << getVersionStr() << std::endl;
 
+  LogThrowIf(R_FAILED(nsInitialize()), "Can't initialize ns services.");
 
+  // Set log level
+  brls::Logger::setLogLevel(brls::LogLevel::INFO);
+
+  LogThrowIf(not brls::Application::init(), "Can't initialize borealis.")
+
+  brls::Application::createWindow("SimpleSwitchBrowser");
+
+  // Have the application register an action on every activity that will quit when you press BUTTON_START
+  brls::Application::setGlobalQuit(true);
+
+  while( brls::Application::mainLoop() );
+
+  nsExit();
   return EXIT_SUCCESS;
 }
